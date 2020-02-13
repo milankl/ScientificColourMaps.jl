6df337157a404fdb000e8327a6bacc8ec37df5da
milankl <milankloewer@gmx.de>
2020-02-13 11:51:07 +0000

    moved data folder to src

--- src/ScientificColourMaps.jl
+++ src/ScientificColourMaps.jl
@@ -1,6 +1,6 @@
 module ScientificColourMaps
 
-    using PyPlot, DelimitedFiles
+    import PyPlot, DelimitedFiles
 
     export acton, bamako, batlow, berlin, bilbao, broc, brocO, buda, cork, corkO, davos, devon, grayC,
             hawaii, imola, lajolla, lapaz, lisbon, nuuk, oleron, oslo, roma, romaO, tofino, tokyo,
